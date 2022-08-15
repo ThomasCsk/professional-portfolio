d9bfd4acc44d1318d5f4e12a2f23799569d87d47
Thomas Ciszek <tciszek515@gmail.com>
2022-08-15 17:33:08 -0400

    about section made

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,21 @@
 import React from "react";
-
+import mugshot from '../assets/Mugshot/mugshot.png';
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section>
+      <div>
+        <div>
+          <img src={mugshot} alt="A selfie of myself"/>
+        </div>
+        <div>
+          <h3>About Me, Thomas Ciszek:</h3>
+        </div>
+      </div>
+      <div>
+        <p>I am a current student, enrolled in the Full Stack Coding Bootcamp through the University of Richmond. As I continue to deveop my skill in a wide variety of different languages and methods, I hope you consider checking out my past works as listed down below!</p>
+        <p>I would emplore you to keep in mind however, that these projects will be changing and getting better as I progress though my course. If one of my projects disappears or is replaced, please be aware that I have deemed it too old and inadequate, compared to my more recent projects.</p>
+      </div>
+    </section>
   );
 };
 
