f60d61d83a36acc9b65c66c9fc6b7cba57307550
Thomas Ciszek <tciszek515@gmail.com>
2022-08-15 16:50:48 -0400

    contact section added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+
 const Contact = () => {
+
   return (
     <div>
-      Contact
+      <h2>Contact</h2>
+      <div>
+        <div>
+          <p>Email: <a href="mailto:tciszek515@gmail.com">tciszek515@gmail.com</a></p>
+        </div>
+        <div> 
+          <p>Phone: (540) 415-2024</p>
+        </div> 
+        <div> 
+          <a href="https://github.com/ThomasCsk" target='_blank' rel="noreferrer">GitHub</a>
+        </div> 
+        <div> 
+          <a href="https://www.linkedin.com/in/thomas-ciszek-05a82322a" target='_blank' rel="noreferrer">LinkedIn</a>
+        </div>
+      </div>
     </div>
   );
 };
